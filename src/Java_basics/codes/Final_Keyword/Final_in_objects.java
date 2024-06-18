718623e744c50ea7e6c1a5f05eb011e1edd0fe94
swayam-12205790 <swayamswarupbarik25@gmail.com>
2024-06-18 18:32:16 +0530

    fourth commit

--- src/Java_basics/codes/Final_Keyword/Final_in_objects.java
+++ src/Java_basics/codes/Final_Keyword/Final_in_objects.java
@@ -10,10 +10,21 @@ public class Final_in_objects {
 
         // changing internal state of object reference by
         //  final reference variable sb
-        sb.append("ForGeeks");
+        sb.append("ForGeeks"); //this property is called non-transitivity
 
         // Again printing the element in StringBuilder
         // object after appending above element in it
         System.out.println(sb);
     }
+//    When a final variable is a reference to an object, then this final variable is called the reference final variable. For example, a final StringBuffer variable looks
+//    defined below as follows:
+//
+//    final StringBuffer sb;
+//
+//    As we all know that a final variable cannot be re-assign. But in the case of a reference final variable, the internal state of the object pointed by that reference
+//    variable can be changed. Note that this is not re-assigning. This property of the final is  called non-transitivity
+//    The non-transitivity property also applies to arrays, because []arrays are objects in Java.Arrays with the
+//    final keyword are also called Final arrays
+//
+
 }
