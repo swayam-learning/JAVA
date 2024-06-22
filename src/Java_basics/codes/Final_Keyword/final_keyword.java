9ffeae20458d037caea96a3b6f5be7f74ab486fd
Swayam Swarup Barik <swayamswarupbarik25@gmail.com>
2024-06-23 01:25:05 +0530

    Update final_keyword.java

--- src/Java_basics/codes/Final_Keyword/final_keyword.java
+++ src/Java_basics/codes/Final_Keyword/final_keyword.java
@@ -12,3 +12,42 @@
 //        Final Classes ------> To Prevent Inheritance
 //    }
 //}
+// Q) Is final method inherited?
+// Ans) Yes, final method is inherited but you cannot override it. For Example:
+
+// class Bike{  
+//   final void run(){System.out.println("running...");}  
+// }  
+// class Honda2 extends Bike{  
+//    public static void main(String args[]){  
+//     new Honda2().run();  
+//    }  
+// // }  
+// Que) Can we initialize blank final variable?
+// Yes, but only in constructor. For example:
+
+// class Bike10{  
+//   final int speedlimit;//blank final variable  
+    
+//   Bike10(){  
+//   speedlimit=70;  
+//   System.out.println(speedlimit);  
+//   }  
+  
+//   public static void main(String args[]){  
+//     new Bike10();  
+//  }  
+// }  
+// static blank final variable
+// A static final variable that is not initialized at the time of declaration is known as static blank final variable. It can be initialized only in static block.
+
+// Example of static blank final variable
+// class A{  
+//   static final int data;//static blank final variable  
+//   static{ data=50;}  
+//   public static void main(String args[]){  
+//     System.out.println(A.data);  
+//  }  
+// }  
+// Q) Can we declare a constructor final?
+// No, because constructor is never inherited.
