b945aa3d700c96767c90eb97d165ec36a029586b
swayam-12205790 <swayamswarupbarik25@gmail.com>
2024-06-23 23:29:49 +0530

    too lazy to commit

---
+++ src/Java_basics/codes/Modifiers/summary.java
@@ -0,0 +1,11 @@
+package Java_basics.codes.Modifiers;
+
+public class summary {
+    public static void main(String[] args) {
+//        Access Modifier	within class	within package	outside package by subclass only	outside package
+//          Private	               Y	            N	                   N	                        N
+//          Default	               Y	            Y	                   N	                        N
+//          Protected	           Y	            Y	                   Y	                        N
+//          Public	               Y	            Y	                   Y	                        Y
+    }
+}
